6bd04a1db5cf27a388c38e3dd61b0de2b65ba21a
johnmondol <johnmondol681@gmail.com>
2022-06-29 10:36:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Harry Porter");
+    searchMovies("Harry");
   }, []);
 
   const searchMovies = async (title) => {
