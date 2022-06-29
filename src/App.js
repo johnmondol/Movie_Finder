c787272ad0ca7130c2e98c304f00c547a33f0fbd
johnmondol <johnmondol681@gmail.com>
2022-06-29 10:30:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com/?apikey=9c34c05";
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9c34c05";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
